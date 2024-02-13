6c9eb2f4fa384b5b3acc5c2186130c75a5a4d7a8
Kyle Roach <kylroa@msn.com>
2024-02-13 14:27:11 -0600

    initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  plugins: ['@typescript-eslint', 'tailwindcss'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+    'plugin:tailwindcss/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'jsx-a11y/anchor-is-valid': [
+      'off',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
+  },
+  settings: {
+    tailwindcss: {
+      callees: ['cn'],
+      config: './tailwind.config.ts',
+    },
+    next: {
+      rootDir: ['./'],
+    },
+  },
+}
+
+module.exports = config
--- next.config.js
+++ next.config.js
@@ -21,11 +21,8 @@ const nextConfig = {
     ],
   },
   experimental: {
-    // TODO: Enable after fixing "Static Bail Out Caught" errors in production
-    // ppr: true,
     esmExternals: "loose",
   },
-  // Already doing linting and typechecking as separate tasks in CI
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 }
