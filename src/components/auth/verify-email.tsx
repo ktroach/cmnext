7b81554a121241fe9c8a6de7e9a65cacdbc08309
Kyle Roach <kylroa@msn.com>
2024-03-07 15:57:52 -0600

    create account

--- src/components/auth/verify-email.tsx
+++ src/components/auth/verify-email.tsx
@@ -21,7 +21,10 @@ import { Input } from '@/components/ui/input'
 import { Icons } from '@/styles/icons'
 import { api } from '@/trpc/client'
 import { toast } from 'sonner'
-import { AttemptEmailAddressVerificationParams, SignUpResource } from '@clerk/types'
+import {
+  AttemptEmailAddressVerificationParams,
+  SignUpResource,
+} from '@clerk/types'
 import { Skeleton } from '../ui/skeleton'
 
 type Inputs = z.infer<typeof verfifyEmailSchema>
@@ -31,29 +34,10 @@ export function VerifyEmailForm() {
   const { isLoaded, signUp, setActive } = useSignUp()
   const [isPending, startTransition] = React.useTransition()
 
-  const createUserAccount = async (signupResource: SignUpResource) => {
-    const userEmail: string | undefined = signupResource?.emailAddress
-      ? signupResource.emailAddress
-      : undefined
-    const userName: string | undefined = signupResource?.username
-      ? signupResource.username
-      : ''
-
-    if (!userName) {
-      throw new Error('Invalid username')
-    }
-
-    if (!userEmail) {
-      throw new Error('Invalid email')
-    }
-
-    const user = await CreateUser(userName, userEmail)
-    console.log("after CreateUser >>> user >>> ", user)
-  }
-
   const createUserMutation = api.users.create.useMutation({
     onSuccess: (newUser) => {
-      console.log("onSuccess >>> newUser >>> ", newUser)
+      console.log('onSuccess >>> newUser >>> ', newUser)
+      // TODO: Do we need this toast to the user?
       toast('User created')
     },
     onError: (error) =>
@@ -64,13 +48,100 @@ export function VerifyEmailForm() {
   })
 
   const isCreatingUser = createUserMutation.isLoading
- 
-  const CreateUser = async (userName: string, userEmail: string) => {
-    console.log("Entered: CreateUser")
+
+  const CreateUser = async (userName: any, userEmail: any) => {
+    console.log('Entered: CreateUser')
     return await createUserMutation.mutateAsync({
       name: userName,
       email: userEmail,
-    });
+    })
+  }
+
+  const createAccountMutation = api.accounts.create.useMutation({
+    onSuccess: (newAccount) => {
+      console.log('onSuccess >>> newAccount >>> ', newAccount)
+      // TODO: Do we need this toast to the user?
+      toast('Account created')
+    },
+    onError: (error) =>
+      toast('Failed to Create Account', {
+        duration: 2000,
+        description: error.message,
+      }),
+  })
+
+  const isCreatingAccount = createAccountMutation.isLoading
+
+  const CreateAccount = async (userName: any, adminId: any) => {
+    console.log('Entered: CreateAccount')
+    return await createAccountMutation.mutateAsync({
+      name: userName,
+      adminId: adminId,
+    })
+  }
+
+  const createUserAccount = async (signupResource: SignUpResource) => {
+    const userEmail: unknown = signupResource?.emailAddress
+      ? signupResource.emailAddress
+      : undefined
+    const userName: unknown = signupResource?.username
+      ? signupResource.username
+      : ''
+
+    // we shouldn't run into this scenario
+    if (!validateUnlikelyScenario(userName, userEmail)) {
+      return
+    }
+
+    const user = await CreateUser(userName, userEmail)
+    console.log('after CreateUser >>> user >>> ', user)
+
+    if (!user) {
+      console.log(
+        'There was a problem creating the user while signing up user: ',
+        userName
+      )
+      toast('There was a problem signing up. Please try again.')
+      return
+    }
+
+    /*
+     * Now we need to create the account that is associated with the user
+     * Account is used for grouping sites and blogs that the user creates.
+     * Sites and blogs are hosted within this application on the same server.
+     * We also need the Account model to store the subscription information and links to payment systems like stripe
+     */
+    const userId = user?.id ? user.id : undefined
+    if (!userId) {
+      console.log('No user.id returned for user ', userEmail)
+      return
+    }
+
+    const userAccount = await CreateAccount(userName, userId)
+
+    if (!userAccount) {
+      console.log(
+        'There was a problem creating the account while signing up user: ',
+        userName
+      )
+      toast('There was a problem signing up. Please try again.')
+      return
+    }
+
+    toast('Account created. Enjoy!')
+  }
+
+  const validateUnlikelyScenario = (userName: unknown, userEmail: unknown) => {
+    if (!userName) {
+      console.log('Invalid username in user signup for ', userEmail)
+      return false
+    }
+
+    if (!userEmail) {
+      console.log('Invalid username in user signup for ', userName)
+      return false
+    }
+    return true
   }
 
   const form = useForm<Inputs>({
@@ -85,8 +156,11 @@ export function VerifyEmailForm() {
 
     startTransition(async () => {
       try {
-        const verificationParams: AttemptEmailAddressVerificationParams = { code: data.code, };
-        const completeSignUp = await signUp.attemptEmailAddressVerification(verificationParams)
+        const verificationParams: AttemptEmailAddressVerificationParams = {
+          code: data.code,
+        }
+        const completeSignUp =
+          await signUp.attemptEmailAddressVerification(verificationParams)
 
         if (completeSignUp.status !== 'complete') {
           console.log(JSON.stringify(completeSignUp, null, 2))
@@ -102,12 +176,17 @@ export function VerifyEmailForm() {
             return
           }
 
-          router.push(`${window.location.origin}/`)
+          router.push(`${window.location.origin}/profile`)
         }
       } catch (err) {
         catchClerkError(err)
       }
     })
+  }
+
+  const handleGoBack = (ev: any) => {
+    ev.stopPropagation()
+    router.push(`${window.location.origin}/signup`)
   }
 
   return (
@@ -140,16 +219,31 @@ export function VerifyEmailForm() {
           <ClerkLoading>
             <Skeleton className="h-10 w-full animate-pulse rounded-full" />
             <Skeleton className="h-10 w-10 animate-pulse rounded-full" />
-          </ClerkLoading>          
+          </ClerkLoading>
           <Button disabled={isPending}>
-            {isPending || isCreatingUser && (
-              <Icons.spinner
-                className="mr-2 h-4 w-4 animate-spin"
-                aria-hidden="true"
-              />
-            )}
+            {isPending ||
+              isCreatingUser ||
+              (isCreatingAccount && (
+                <Icons.spinner
+                  className="mr-2 h-4 w-4 animate-spin"
+                  aria-hidden="true"
+                />
+              ))}
             Create Account
             <span className="sr-only">Create Account</span>
+          </Button>
+
+          <Button disabled={isPending} onClick={handleGoBack}>
+            {isPending ||
+              isCreatingUser ||
+              (isCreatingAccount && (
+                <Icons.spinner
+                  className="mr-2 h-4 w-4 animate-spin"
+                  aria-hidden="true"
+                />
+              ))}
+            Back to Signup
+            <span className="sr-only">Back to Signup</span>
           </Button>
         </form>
       </Form>
