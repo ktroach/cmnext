f04c19a37d09421e49c05bf46d0fb50e0e89a14d
Kyle Roach <kylroa@msn.com>
2024-03-08 15:14:24 -0600

    core

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from '@/components/theme-provider'
 import { TRPCReactProvider } from '@/trpc/provider'
-import { fontMono, fontSans } from '@/styles/fonts'
+import { fontSans, KaiseiDecolRegular, TheLastShuriken } from '@/styles/fonts'
 import '@/styles/globals.css'
 
 interface RootLayoutProps {
@@ -18,9 +18,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <head />
           <body
             className={cn(
-              'min-h-screen bg-background font-sans antialiased',
+              'min-h-screen bg-background',
+              `font-sans antialiased`,
               fontSans.variable,
-              fontMono.variable
             )}
           >
             <TRPCReactProvider>
